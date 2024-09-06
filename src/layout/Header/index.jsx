ac9200c1a3975f0c81c9ea33b753a4d64a83d1eb
Néphélim <nephcohen@gmail.com>
2024-09-06 09:16:39 +0200

    mentorat

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -5,35 +5,33 @@ import logo from '../../assets/common/logo.png';
 const Header = () => {
   return (
     <header className="header">
-      <div className="container">
+      <div className="logo">
         <img src={logo} className="header__logo" />
       </div>
-      <div>
-        <nav className="header__nav">
-          <ul className="header__nav-list">
-            <li className="header__nav-item">
-              <a href="/" className="header__nav-link">
-                Accueil
-              </a>
-            </li>
-            <li className="header__nav-item">
-              <a href="#" className="header__nav-link">
-                Profil
-              </a>
-            </li>
-            <li className="header__nav-item">
-              <a href="#" className="header__nav-link">
-                Réglages
-              </a>
-            </li>
-            <li className="header__nav-item">
-              <a href="#" className="header__nav-link">
-                Communauté
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className="header__nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/" className="header__nav-link">
+              Accueil
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a href="#" className="header__nav-link">
+              Profil
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a href="#" className="header__nav-link">
+              Réglage
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a href="#" className="header__nav-link">
+              Communauté
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
